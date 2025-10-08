6a4daf5bd8a1b22145f35be7b9c4f37d5a90f258
Ван цзялэ <571899823@qq.com>
2025-10-08 21:30:37 +0800

    Create График функции

---
+++ График функции.py
@@ -0,0 +1,10 @@
+for i in range(1,21):
+    print("|",end="")
+    if (21-i)%2==0:
+        for j in range(1,(21-i)//2):
+            print(" ",end="")
+        print("\x1b[38;5;27m*\x1b[0m",end="")
+    else:
+        print("")
+for k in range(1,20):
+    print("——",end=" ")
